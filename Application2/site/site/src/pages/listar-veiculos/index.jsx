cda795c5cc77247f3c0fb2891e690589feef4f59
Renatha Loiza Monteiro e Silva <renatha.loiza@hotmail.com>
2024-01-24 22:10:06 -0300

    criando tela

--- Application2/site/site/src/pages/listar-veiculos/index.jsx
+++ Application2/site/site/src/pages/listar-veiculos/index.jsx
@@ -1,20 +1,46 @@
 import React from 'react';
 
 function Index() {
+    const [veiculos, SetVeiculos] = useState([]);
+    useEffect(() => {
+        //todo criar funçao para buscar veiculos
+        async function getVeiculos() {
+            try {
+                const response = await fetch('https://localhost:7113/api/Veiculo/Veiculos-Disponiveis')
+                const data = await response.json();
+                console.log(data);
+                SetVeiculos(data);
+
+            }
+            catch (error) {
+                console.erro("Erro ao obter veiculo", error);
+            }
+
+        }
+        getVeiculos();
+    }, []);
     return (
-        <div className="card">
-            <img src="caminho_da_imagem" alt="veiculo"></img>
-            <div className="card-content">
-                <h2>Tipo de veiculo</h2>
-                <p>Preço R$2000</p>
-                <p>Ano:2022</p>
-                <p>Estado: São Paulo</p>
-            </div>
-
-
-
+        <div className="veiculo-container">
+            {
+                veiculos.map(veiculo =>(
+                    <div key={veiculo.id} className="card"> 
+                        <img src={veiculo.imagem} alt={'veiculo ${veiculo.tipoVeiculo}'} />
+                        <br/>
+                        <div className="card-content">
+                            <p>Tipo de veiculo:{veiculo.tiposdeveiculos }</p>
+                            <p>Estado:{veiculo.estado }</p>
+                            <p>Ano:{veiculo.anoFabricacao }</p>
+                            <p>placa:{veiculo.placa }</p>
+                    </div>
+                 
+                    </div>
+            ))
+           }
         </div>
+            
   );
 }
+
+
 
 export default Index;
